cc3eb770fa1115d0c101bf6299b188fe9827efe6
Remixer Dec <6587642+remixer-dec@users.noreply.github.com>
2023-10-07 01:31:02 +0400

    Interactive access management

--- modules/admin.py
+++ modules/admin.py
@@ -1,6 +1,7 @@
 from aiogram.filters import Command, CommandObject
 from aiogram.types import Message
 from utils import parse_photo, log_exceptions
+from config_reader import config
 import logging
 logger = logging.getLogger(__name__)
 
@@ -23,3 +24,35 @@ class AdminModule:
       msg = message if not message.reply_to_message else message.reply_to_message
       prefix = '[reply info]\n' if message.reply_to_message else ''
       await message.reply(f'{prefix}Chat ID: {msg.chat.id}\nUser ID: {msg.from_user.id}')
+    
+    @dp.message(Command(commands=["ban", "unban"]), flags={"admins_only": True})
+    @log_exceptions(logger)
+    async def ban_unban_user(message: Message, command: CommandObject) -> None:
+      user = False
+      if message.reply_to_message:
+        user = message.reply_to_message.from_user
+        user_id = user.id
+      else:
+        try:
+          user_id = int(str(command.args))
+        except Exception:
+          return await message.reply('Incorrect user id')
+      if command.command == "ban":
+        if user_id == bot._me.id:
+          return await message.reply('Funny!')
+        if user_id in config.adminlist:
+          return await message.reply('Unable ban an admin')
+        if user_id not in config.blacklist:
+          config.blacklist.append(user_id)
+          config.blacklist = config.blacklist
+          who = str(user_id) if not user else user.first_name + f" ({str(user_id)})"
+          await message.reply(f'{who} has been banned')
+        else:
+          await message.reply('User is already banned')          
+      else:
+        if user_id in config.blacklist:
+          config.blacklist.remove(user_id)
+          config.blacklist = config.blacklist
+          await message.reply('User unbanned')
+        else:
+          await message.reply('User is not banned')
