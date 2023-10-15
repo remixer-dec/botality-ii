a6ad18a6eb17095f486586286b11f607862a5ea0
Remixer Dec <6587642+remixer-dec@users.noreply.github.com>
2023-10-15 20:58:33 +0400

    Fixed saiga support

--- characters/ru_saiga_lcpp.py
+++ characters/ru_saiga_lcpp.py
@@ -31,25 +31,25 @@ def get_system_tokens(model):
 
 def get_assistant_variables():
   # change these only if your custom lora input format changed
-  return {'replace_username': 'User'}
+  return {}
 
 def get_chat_variables(context=None):
   # change these as you wish
-  name = 'Saiga'
+  name = 'Сайга'
   intro = SYSTEM_PROMPT
   return {"intro": intro, "personality": '', 'name': name, 'pre_dialog': '', **get_assistant_variables() }
 
 def get_generation_config(override={}):
   return {
-    "temperature": 0.2,
-    "top_k": 30,
+    "temperature": 0.7,
+    "top_k": 50,
     "top_p": 0.9,
     "repetition_penalty": 1.1,
     **override
   }
 
 def custom_input_formatter(chronicler, details, fresh=True):
-  model = details['model']['instance']
+  model = details['model'].model
   tokens = get_system_tokens(model)
   message_tokens = get_message_tokens(model=model, role="user", content=details['message'])
   role_tokens = [model.token_bos(), BOT_TOKEN, LINEBREAK_TOKEN]
