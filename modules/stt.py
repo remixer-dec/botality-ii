365d0b41f8bf08184b9ffcb66cd6af3e044ba7a5
Remixer Dec <6587642+remixer-dec@users.noreply.github.com>
2023-11-28 01:32:43 +0400

    Move stt autoreply to extensions

--- modules/stt.py
+++ modules/stt.py
@@ -17,25 +17,6 @@ class SpeechToTextModule:
     self.bot = bot
     self.model = active_model.init()
     self.cache = {}
-    
-    if config.stt_autoreply_mode: # TODO: move this to separate module after implementing extensions
-      @dp.message((F.voice), flags={"long_operation": "record_audio"})
-      async def handle_voice_messages(message: Message):
-        error, text = await self.recognize_voice_message(message)
-        if text and 'llm' in config.active_modules:
-          llm = dp.modules['llm']
-          tts = dp.modules['tts']
-          llm_call_func = llm.assist if config.stt_autoreply_mode == 'assist' else llm.chat
-          reply = await llm_call_func(text, llm.get_common_chat_attributes(message))
-          if 'tts' in config.active_modules:
-            voice = config.stt_autoreply_voice or tts.voices[0]
-            voice = random.choice(tts.voices) if voice == 'random' else voice
-            voice = self.cache.setdefault(message.from_user.id, voice if type(voice) is str else voice['voice'])
-            await bot.reply_tts(message=message, command=SimpleNamespace(command=voice, args=[reply]))
-          else:
-            return await message.answer(reply)
-        if error:
-          return await message.answer(f"Error, <b>{error}</b>")
 
     @dp.message(Command(commands=["stt", "recognize", "transcribe"]), flags={"long_operation": "typing"})
     async def command_stt_handler(message: Message, command: CommandObject) -> None:
